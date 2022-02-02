75e19e72b1ffb8ea5c56611bb2c26192aca6325a
Anthony-Lei <48465180+Anthony-Lei@users.noreply.github.com>
2022-02-02 14:01:41 +0800

    Validate input data and style invalid with classes

--- styled_components/src/components/CourseGoals/CourseInput/CourseInput.jsx
+++ styled_components/src/components/CourseGoals/CourseInput/CourseInput.jsx
@@ -5,21 +5,28 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = event => {
-    setEnteredValue(event.target.value);
+    const value = event.target.value;
+    if (value.trim().length > 0) setIsValid(true);
+    setEnteredValue(value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={`form-control ${!isValid && "invalid"}`}>
         <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <input value={enteredValue} type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>

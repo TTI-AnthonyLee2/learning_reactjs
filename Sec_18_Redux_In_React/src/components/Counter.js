710a2c7c13efa7f3bf0616d0193f2f9fd35ed16e
anthony_lee2 <anthonyleetti@gmail.com>
2022-02-22 18:05:04 +0800

    Practice react-redux in functional components

--- Sec_18_Redux_In_React/src/components/Counter.js
+++ Sec_18_Redux_In_React/src/components/Counter.js
@@ -1,12 +1,28 @@
 import classes from './Counter.module.css';
+import { useSelector, useDispatch } from 'react-redux';
 
 const Counter = () => {
+  const counter = useSelector(state => state.counter);
+  const dispatch = useDispatch();
+
   const toggleCounterHandler = () => {};
+
+  const handleIncrement = () => {
+    dispatch({type: "INCREMENT"});
+  };
+
+  const handleDecrement = () => {
+    dispatch({type: "DECREMENT"});
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={handleIncrement}>Increment</button>
+        <button onClick={handleDecrement}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );

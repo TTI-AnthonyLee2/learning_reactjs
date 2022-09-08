fb7297cb16e3e8bb6c691dbd9ec42fe50d142785
BH\anthony.lee <anthony.lee@omnisense-systems.com>
2022-09-08 18:08:48 +0800

    Sec.10: manage logout handler with Context

--- Sec_10_useEffect_useReducer_contextAPI/src/store/auth-context.js
+++ Sec_10_useEffect_useReducer_contextAPI/src/store/auth-context.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
-const AuthContext = React.createContext({
-  isLoggedIn: false, // set a default value, but it will only be adopted when we use 'Context.Consumer' alone without 'Context.Provider'
+const AuthContext = React.createContext({ // set a default value, but it will only be adopted when we use 'Context.Consumer' alone without 'Context.Provider'
+  isLoggedIn: false,
+  onLogout: () => {} 
 });
 
 export default AuthContext;

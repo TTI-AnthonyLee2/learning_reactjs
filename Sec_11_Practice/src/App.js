f79f1906669b65376fadd53970506b9cf245e178
BH\anthony.lee <anthony.lee@omnisense-systems.com>
2022-09-13 10:24:34 +0800

    Sec.11: introduce createGlobalStyle
    Replace index.css with using createGlobalStyle

--- Sec_11_Practice/src/App.js
+++ Sec_11_Practice/src/App.js
@@ -1,11 +1,13 @@
 import Header from './components/Layout/Header';
 
+import GlobalStyles from './styles/Global';
 import { ThemeProvider } from 'styled-components';
 import theme from './theme';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyles />
       <Header />
     </ThemeProvider>
   );

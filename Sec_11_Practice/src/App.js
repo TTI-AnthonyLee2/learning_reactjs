81236af3df72ea4ec818e469a11e1cea3d2d22fc
BH\anthony.lee <anthony.lee@omnisense-systems.com>
2022-09-13 12:45:25 +0800

    Sec.11: use react-helmet to inject css font
    Please do not use @import CSS syntax in createGlobalStyle
    at this time, as the CSSOM APIs we use in production do not
    handle it well.
    Instead, we recommend using a library such as react-helmet
    to inject a typical <link> meta tag to the stylesheet, or simply
    embedding it manually in your index.html <head> section for
    a simpler app.

--- Sec_11_Practice/src/App.js
+++ Sec_11_Practice/src/App.js
@@ -1,5 +1,6 @@
 import Header from './components/Layout/Header';
 
+import { Helmet } from 'react-helmet';
 import GlobalStyles from './styles/Global';
 import { ThemeProvider } from 'styled-components';
 import theme from './theme';
@@ -7,6 +8,11 @@ import theme from './theme';
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <Helmet>
+        <style>
+          @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap');
+        </style>
+      </Helmet>
       <GlobalStyles />
       <Header />
     </ThemeProvider>

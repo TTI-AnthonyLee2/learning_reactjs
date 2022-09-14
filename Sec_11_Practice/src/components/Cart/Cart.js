17bf1349cc12eeaf9ebf1643310dbd72685eb468
BH\anthony.lee <anthony.lee@omnisense-systems.com>
2022-09-14 15:28:31 +0800

    Sec.11: add the Cart component

---
+++ Sec_11_Practice/src/components/Cart/Cart.js
@@ -0,0 +1,25 @@
+import { StyledCartActions, StyledCartItems, StyledCartTotal } from '../../styles/Cart.styled';
+
+const Cart = () => {
+  const cartItems = [{id: 'c1', name: 'Sushi', amount: 2, price: 12.99}].map(item => (
+    <li>{item.name}</li>
+  ));
+
+  return (
+    <div>
+      <StyledCartItems>
+        {cartItems}
+      </StyledCartItems>
+      <StyledCartTotal>
+        <span>Total Amount</span>
+        <span>100</span>
+      </StyledCartTotal>
+      <StyledCartActions>
+        <button>Close</button>
+        <button>Order</button>
+      </StyledCartActions>
+    </div>
+  );
+}
+
+export default Cart;

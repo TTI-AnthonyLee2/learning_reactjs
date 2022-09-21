7fa6af56ae26bbda3e299127cfe42d6eec557910
BH\anthony.lee <anthony.lee@omnisense-systems.com>
2022-09-21 10:52:16 +0800

    Sec.12: demo state-triggered re-executing
    Also see the real DOM doesn't re-render every time because
    of virtual DOM mechanism.

--- Sec_12_useCallback_useMemo/src/App.js
+++ Sec_12_useCallback_useMemo/src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Button from './components/UI/Button/Button';
 
 import './App.css';
 
 function App() {
+  const [showMsg, setShowMsg] = useState(false);
+
+  const clickHandler = () => {
+    setShowMsg(prev => !prev);
+  }
+
+  console.log('[App] executing!');
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <Button onClick={clickHandler}>show messages</Button>
+      {showMsg && <p>here is a message!</p>}
     </div>
   );
 }

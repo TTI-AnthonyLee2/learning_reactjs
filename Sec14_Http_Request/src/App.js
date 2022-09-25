d401a53c7fa5b52c21ab947ae70e49d4526da149
anthony.lei <anthonylei@Anthonys-MacBook-Pro.local>
2022-09-25 17:14:34 +0800

    Sec.14: send a request via fetch api
    Demo how to handle async data fetching with React.

--- Sec14_Http_Request/src/App.js
+++ Sec14_Http_Request/src/App.js
@@ -1,31 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [moviesList, setMoviesList] = useState([]);
+  const fetchMoviesHandler = () => {
+    fetch('https://swapi.dev/api/films')
+      .then(response => response.json())
+      .then(data => {
+        const moviesInfo = data.results.map(movie => ({
+          id: movie.episode_id,
+          title: movie.title,
+          openingText: movie.opening_crawl,
+          releaseDate: movie.release_date
+        }));
+        
+        setMoviesList(moviesInfo);
+      });
+  }
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={moviesList} />
       </section>
     </React.Fragment>
   );

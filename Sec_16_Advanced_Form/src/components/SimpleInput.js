d39236584d943dddfb5d56391341d5f66f724797
anthony.lei <anthonylei@Anthonys-MacBook-Pro.local>
2022-10-08 23:34:49 +0800

    Sec.16: handle the input with useState or useRef

--- Sec_16_Advanced_Form/src/components/SimpleInput.js
+++ Sec_16_Advanced_Form/src/components/SimpleInput.js
@@ -1,9 +1,35 @@
+import { useRef, useState } from 'react';
+
 const SimpleInput = (props) => {
+  const [enteredName, setenteredName] = useState('');
+  const nameInputRef = useRef();
+
+  const inputChangedHandler = e => {
+    setenteredName(e.target.value);
+  }
+
+  const submitHandler = e => {
+    e.preventDefault();
+
+    console.log('useState: ' + enteredName);
+    setenteredName('');
+
+    console.log('useRef: ' + nameInputRef.current.value);
+  }
+
+  console.log('re-executing.')
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input 
+          ref={nameInputRef}
+          type='text' 
+          id='name' 
+          value={enteredName}
+          onChange={inputChangedHandler} 
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>

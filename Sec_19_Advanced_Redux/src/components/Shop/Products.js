e99f255b9fb816e73a6fd7e10b5488656109b48d
Anthony Lei <hkbornguy2@gmail.com>
2022-10-24 16:50:50 +0800

    Sec.19: create a dummy products list

--- Sec_19_Advanced_Redux/src/components/Shop/Products.js
+++ Sec_19_Advanced_Redux/src/components/Shop/Products.js
@@ -1,17 +1,37 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODUCTS = [
+  {
+    id: 'p1',
+    title: 'Test 1',
+    price: 6,
+    description: 'This is a first product - amazing!'
+  },
+  {
+    id: 'p2',
+    title: 'Test 2',
+    price: 8.33,
+    description: 'This is a second product - amazing!'
+  },
+];
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          id='p1'
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {
+          DUMMY_PRODUCTS.map(({ id, title, price, description }) => (
+            <ProductItem
+              key={id}
+              id={id}
+              title={title}
+              price={price}
+              description={description}
+            />
+          ))
+        }
       </ul>
     </section>
   );

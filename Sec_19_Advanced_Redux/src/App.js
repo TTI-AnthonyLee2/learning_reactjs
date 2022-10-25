b053763e5b47665c75e6df7c94bf4a7055039a80
Anthony Lei <hkbornguy2@gmail.com>
2022-10-25 17:27:22 +0800

    Sec.19: store cart data to database
    We use the useEffect hook to handle side-effects which save cart data
    via fetch API to a database provided by Firebase.

--- Sec_19_Advanced_Redux/src/App.js
+++ Sec_19_Advanced_Redux/src/App.js
@@ -1,10 +1,21 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
+console.log('App component...');
+
 function App() {
   const showCart = useSelector(state => state.cart.showCart);
+  const cartItemList = useSelector(state => state.cart.cartItemList);
+
+  useEffect(() => {
+    fetch('https://react-http-a4628-default-rtdb.firebaseio.com/cart.json', {
+      method: 'PUT',
+      body: JSON.stringify(cartItemList)
+    });
+  }, [cartItemList]);
 
   return (
     <Layout>

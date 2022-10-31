5765438fc13cbbcbcaf4796c018a2839bc6bddf7
Anthony Lei <hkbornguy2@gmail.com>
2022-10-31 18:47:45 +0800

    Sec.20.1: create quotes slice

--- Sec_20_1_React_Router_Practice/src/pages/AllQuotes.js
+++ Sec_20_1_React_Router_Practice/src/pages/AllQuotes.js
@@ -1,23 +1,27 @@
+import { useSelector } from "react-redux";
+
 import QuoteList from "../components/quotes/QuoteList";
 
-const DUMMY_QUOTES = [
-  {
-    id: 'q1',
-    author: 'John',
-    text: 'This is a good choice.'
-  },
-  {
-    id: 'q2',
-    author: 'John',
-    text: 'This is a good choice.'
-  },
-];
+// const DUMMY_QUOTES = [
+//   {
+//     id: 'q1',
+//     author: 'John',
+//     text: 'This is a good choice.'
+//   },
+//   {
+//     id: 'q2',
+//     author: 'John',
+//     text: 'This is a good choice.'
+//   },
+// ];
 
 const AllQuotes = () => {
+  const quotesList = useSelector(state => state.quotes.quotesList);
+
   return (
     <>
       <h1>All Quotes</h1>
-      <QuoteList quotes={DUMMY_QUOTES} />
+      <QuoteList quotes={quotesList} />
     </>
   );
 };

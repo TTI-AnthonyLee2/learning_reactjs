1232159d009840f7fbca7d97c77c1dbfbf9a4c0a
Anthony Lei <hkbornguy2@gmail.com>
2022-10-31 15:55:29 +0800

    Sec.20.1: add the quote detail page

--- Sec_20_1_React_Router_Practice/src/App.js
+++ Sec_20_1_React_Router_Practice/src/App.js
@@ -3,15 +3,18 @@ import { Route, Switch } from 'react-router-dom';
 import AllQuotes from './pages/AllQuotes';
 import classes from './components/layout/Layout.module.css';
 import NewQuote from './pages/NewQuote';
+import QuoteDetail from './pages/QuoteDetail';
 
 function App() {
   return (
     <main className={classes.main}>
       <Switch>
-        <Route path='/quotes'>
+        <Route path='/quotes' exact>
           <AllQuotes />
         </Route>
-        <Route path='/quotes/:quoteId'></Route>
+        <Route path='/quotes/:quoteId'>
+          <QuoteDetail />
+        </Route>
         <Route path='/new-quote'>
           <NewQuote />
         </Route>
--- Sec_20_1_React_Router_Practice/src/pages/QuoteDetail.js
+++ Sec_20_1_React_Router_Practice/src/pages/QuoteDetail.js
@@ -1,7 +1,12 @@
+import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
 const QuoteDetail = () => {
   return (
     <>
       <h1>Quote Detail</h1>
+      <HighlightedQuote />
+      <Comments />
     </>
   );
 };

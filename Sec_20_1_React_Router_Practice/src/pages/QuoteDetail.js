6fede01721d2319c2c644237b66ee9b8264d6280
Anthony Lei <hkbornguy2@gmail.com>
2022-10-31 16:21:43 +0800

    Sec.20.1: extract the quote id

--- Sec_20_1_React_Router_Practice/src/pages/QuoteDetail.js
+++ Sec_20_1_React_Router_Practice/src/pages/QuoteDetail.js
@@ -1,7 +1,13 @@
+import { useParams } from "react-router-dom";
+
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
 const QuoteDetail = () => {
+  const { quoteId } = useParams();
+
+  console.log(quoteId);
+
   return (
     <>
       <h1>Quote Detail</h1>

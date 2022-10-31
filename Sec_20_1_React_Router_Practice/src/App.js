c42ec38d59762573c756e70e80bab03854833dcc
Anthony Lei <hkbornguy2@gmail.com>
2022-10-31 22:59:10 +0800

    Sec.20.1: add a navigation bar

--- Sec_20_1_React_Router_Practice/src/App.js
+++ Sec_20_1_React_Router_Practice/src/App.js
@@ -4,25 +4,29 @@ import AllQuotes from './pages/AllQuotes';
 import classes from './components/layout/Layout.module.css';
 import NewQuote from './pages/NewQuote';
 import QuoteDetail from './pages/QuoteDetail';
+import MainNavigation from './components/layout/MainNavigation';
 
 function App() {
   return (
-    <main className={classes.main}>
-      <Switch>
-        <Route path='/' exact>
-          <Redirect to='/quotes'/>
-        </Route>
-        <Route path='/quotes' exact>
-          <AllQuotes />
-        </Route>
-        <Route path='/quotes/:quoteId'>
-          <QuoteDetail />
-        </Route>
-        <Route path='/new-quote'>
-          <NewQuote />
-        </Route>
-      </Switch>
-    </main>
+    <>
+      <MainNavigation />
+      <main className={classes.main}>
+        <Switch>
+          <Route path='/' exact>
+            <Redirect to='/quotes'/>
+          </Route>
+          <Route path='/quotes' exact>
+            <AllQuotes />
+          </Route>
+          <Route path='/quotes/:quoteId'>
+            <QuoteDetail />
+          </Route>
+          <Route path='/new-quote'>
+            <NewQuote />
+          </Route>
+        </Switch>
+      </main>
+    </>
   );
 }
 

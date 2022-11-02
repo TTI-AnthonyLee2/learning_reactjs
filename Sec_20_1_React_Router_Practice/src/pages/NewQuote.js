5f1d5862581702892b3c1234755013293a881633
Anthony Lei <hkbornguy2@gmail.com>
2022-11-02 17:44:00 +0800

    Sec.20.1: use useHistory to redirect pages

--- Sec_20_1_React_Router_Practice/src/pages/NewQuote.js
+++ Sec_20_1_React_Router_Practice/src/pages/NewQuote.js
@@ -1,12 +1,16 @@
 import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
 import QuoteForm from "../components/quotes/QuoteForm";
 import { quotesActions } from '../store/quotesSlice';
 
 const NewQuote = () => {
   const dispatch = useDispatch();
+  const histroy = useHistory();
 
   const addQuoteHandler = (quote) => {
     dispatch(quotesActions.addQuote(quote));
+    histroy.push('/quotes'); 
   }
 
   return (

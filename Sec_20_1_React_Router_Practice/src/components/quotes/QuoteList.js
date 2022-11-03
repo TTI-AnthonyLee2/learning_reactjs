46378801bf86d6de6312e84d871823edf268287b
Anthony Lei <hkbornguy2@gmail.com>
2022-11-03 13:49:24 +0800

    Sec.20.1: add the quotes sorting

--- Sec_20_1_React_Router_Practice/src/components/quotes/QuoteList.js
+++ Sec_20_1_React_Router_Practice/src/components/quotes/QuoteList.js
@@ -1,23 +1,60 @@
 import { Fragment } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
 
 import NoQuotesFound from './NoQuotesFound';
 import QuoteItem from './QuoteItem';
 import classes from './QuoteList.module.css';
 
+const SORT_TYPE = {
+  ascending: 'asc',
+  descending: 'desc'
+}
+
+const sortQuotes = (quotes, isAscending) => {
+  return quotes.sort((a, b) => {
+    console.log(a);
+    console.log(b);
+    if (isAscending) {
+      return a.id > b.id ? 1 : -1;
+    } else {
+      return a.id < b.id ? 1 : -1;
+    }
+  });
+}
+
 const QuoteList = ({ quotes }) => {
+  const history = useHistory();
+  const { search } = useLocation();
+  
   const isNoQuote = quotes.length === 0;
   
+  const queryParams = new URLSearchParams(search);
+  const isSortingAscending = queryParams.get('sort') === SORT_TYPE.ascending;
+  
+  // Array.sort() method will mutate the original quotes, and the quotes from Redux CANNOT be mutated.
+  // So we do a shallow copy for quotes using spread syntax to avoid this.
+  const sortedQuotes = sortQuotes([...quotes], isSortingAscending);
+  
+  const changeSortingHandler = () => {
+    history.push(`/quotes?sort=${isSortingAscending ? SORT_TYPE.descending : SORT_TYPE.ascending}`);
+  };
+
   return (
     <Fragment>
       {
         isNoQuote ? 
         <NoQuotesFound />
         : 
-        <ul className={classes.list}>
-          {quotes.map((quote) => (
-            <QuoteItem key={quote.id} {...quote} />
-          ))}
-        </ul>
+        <>
+          <div className={classes.sorting}>
+            <button onClick={changeSortingHandler}>Sort {isSortingAscending ? 'Descending' : 'Ascending'}</button>
+          </div>
+          <ul className={classes.list}>
+            {sortedQuotes.map((quote) => (
+              <QuoteItem key={quote.id} {...quote} />
+            ))}
+          </ul>
+        </>
       }
     </Fragment>
   );
--- Sec_20_1_React_Router_Practice/src/pages/AllQuotes.js
+++ Sec_20_1_React_Router_Practice/src/pages/AllQuotes.js
@@ -2,19 +2,6 @@ import { useSelector } from "react-redux";
 
 import QuoteList from "../components/quotes/QuoteList";
 
-// const DUMMY_QUOTES = [
-//   {
-//     id: 'q1',
-//     author: 'John',
-//     text: 'This is a good choice.'
-//   },
-//   {
-//     id: 'q2',
-//     author: 'John',
-//     text: 'This is a good choice.'
-//   },
-// ];
-
 const AllQuotes = () => {
   const quotesList = useSelector(state => state.quotes.quotesList);
 
